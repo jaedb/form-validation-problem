e1bb363b8bb7af8204693b80f6bc8db13760e525
James Barnsley <james@barnsley.nz>
2019-01-14 21:03:22 +1300

    In-form error messages, BEM basic

--- dist/index.js
+++ dist/index.js
@@ -10486,6 +10486,11 @@ $(document).ready(function(){
 		// form submission
 		var valid = true;
 		form.find('.error').removeClass('error');
+		form.find('.field--error').removeClass('field--error');
+
+		// Delete all our field messages (if any)
+		// This gives us a clean slate for this next submission attempt
+		form.find('.field__message').remove();
 
 		/**
 		 * Validate email field
@@ -10493,21 +10498,21 @@ $(document).ready(function(){
 		 * Must follow email format 
 		 **/
 		if (form.find('#email').length > 0){
-			var field = form.find('#email');			
+			var input = form.find('#email');
+			var field = input.closest('.field');
 
 			// Emptiness check
-			if (!field.val() || field.val() === "") {
-				console.debug("Email cannot be empty");
+			if (!input.val() || input.val() === "") {
 				valid = false;
-				field.parent().addClass('error');
-			}
+				field.append('<div class="field__message field__message--bad">Email cannot be empty</div>');
+				field.addClass('error field--error');
 
 			// Invalid email format, based on proven email validation standards. 
 			// See https://emailregex.com/email-validation-summary/ 
-			if (!new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(field.val())) {
-				console.debug("Email not correct format");
+			} else if (!new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(input.val())) {
 				valid = false;
-				field.parent().addClass('error');
+				field.append('<div class="field__message field__message--bad">Email not correct format</div>');
+				field.addClass('error field--error');
 			}
 		}
 
@@ -10517,20 +10522,21 @@ $(document).ready(function(){
 		 * Must be greater than 8 characters
 		 **/
 		if (form.find('#password').length > 0){
-			var field = form.find('#password');			
+			var input = form.find('#password');
+			var field = input.closest('.field');
 
 			// Emptiness check
-			if (!field.val() || field.val() === "") {
-				console.debug("Password cannot be empty");
+			if (!input.val() || input.val() === "") {
 				valid = false;
-				field.parent().addClass('error');
+				field.append('<div class="field__message field__message--bad">Password cannot be empty</div>');
+				field.addClass('error field--error');
 			}
 
 			// Shortness check
-			if (field.val().length < 8) {
-				console.debug("Password must be greater than 8 characters");
+			if (input.val().length < 8) {
 				valid = false;
-				field.parent().addClass('error');
+				field.append('<div class="field__message field__message--bad">Password must be greater than 8 characters</div>');
+				field.addClass('error field--error');
 			}
 		}
 
@@ -10540,13 +10546,14 @@ $(document).ready(function(){
 		 * Must have a value selected
 		 **/
 		if (form.find('#colour').length > 0){
-			var field = form.find('#colour');			
+			var input = form.find('#colour');	
+			var field = input.closest('.field');		
 
 			// Emptiness check
-			if (!field.val() || field.val() === "") {
-				console.debug("Colour cannot be empty");
+			if (!input.val() || input.val() === "") {
 				valid = false;
-				field.parent().addClass('error');
+				field.append('<div class="field__message field__message--bad">Colour cannot be empty</div>');
+				field.addClass('error field--error');
 			}
 		}
 
@@ -10556,9 +10563,11 @@ $(document).ready(function(){
 		 * Must have two or greater selections
 		 **/
 		if (form.find('input[name="animal"]:checked').length < 2){
-			console.debug("Must select at least 2 animals");
 			valid = false;
-			form.find('input[name="animal"]').parent().addClass('error');
+
+			var field = form.find('input[name="animal"]').closest('.field');
+			field.append('<div class="field__message field__message--bad">Must select at least 2 animals</div>');
+			field.addClass('error field--error');
 		}
 
 
@@ -10570,30 +10579,33 @@ $(document).ready(function(){
 		if (form.find('input[name="animal"][value="tiger"]:checked').length > 0){
 
 			if (form.find('#tiger_type').length > 0){
-				var field = form.find('#tiger_type');			
+				var input = form.find('#tiger_type');
+				var field = input.closest('.field');
 
 				// Emptiness check
-				if (!field.val() || field.val() === "") {
-					console.debug("Type cannot be empty when Tiger is selected");
+				if (!input.val() || input.val() === "") {
 					valid = false;
-					field.parent().addClass('error');
+					field.append('<div class="field__message field__message--bad">Type cannot be empty when Tiger is selected</div>');
+					field.addClass('error field--error');
 				}
 			} else {				
-				console.error("Tiger selected, but Type field not found");
 				valid = false;
-				form.find('input[name="animal"]').parent().addClass('error');
+				console.error("Tiger selected, but Type field not found");
 			}
 		}
 
 
 		// Validation failed; halt submission and alert the user
 		if (!valid){
-			alert("Validation failed. Please check fields and try again.");
 			e.preventDefault();
 
 		// Valid; proceed to submit
 		} else {
-			console.debug("Validation successful... proceeding with submission.");
+			form.find('input[type="submit"]').val('Submitting').setAttribute('disabled','disabled');
+
+			// TESTING
+			e.preventDefault();
+
 			return;
 		}
 	});
